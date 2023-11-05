5610e1cfa41e9767a83df700bef5620cfbd46793
Cass Dahle <isaacbd.personal@gmail.com>
2023-11-04 20:04:28 -0700

    got the BDD for even and prime sets

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ from functions import *
 even = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
 prime = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
 
-# print(number_to_boolean_formula(3));
-print(number_to_boolean_formula(10))
+# get the BDD for the set of even numbers
+even_boolean_formula = list_to_boolean_formula(even)
+even_BDD = expr2bdd(even_boolean_formula)
+
+# get the BDD for the set of prime numbers
+prime_boolean_formula = list_to_boolean_formula(prime)
+prime_BDD = expr2bdd(prime_boolean_formula)
