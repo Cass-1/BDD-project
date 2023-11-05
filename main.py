cb3b3d6cde3398e31cda6a66df50900c667a17d6
Cass Dahle <isaacbd.personal@gmail.com>
2023-11-04 20:11:36 -0700

    Used list comprehnsion for even and edge sets

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ The main program for my CS 350 BDD project
 from my_functions import *
 
 # even and prime arrays
-even = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+even = [i for i in range(32) if i%2 is 0]
 prime = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+
+# edge array
+edges = [(i,j) for i in range(32) for j in range(32) if (i+3)%32 is j%32 or (i+8)%32 is j%32]
 
 # get the BDD for the set of even numbers
 even_boolean_formula = list_to_boolean_formula(even)
