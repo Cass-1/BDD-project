257dc6ac6719807a666dd039d0f50c06977cdf16
Cass Dahle <isaacbd.personal@gmail.com>
2023-11-04 19:40:54 -0700

    Finished number_to_boolean_formula()

--- bdd.py
+++ bdd.py
@@ -1,12 +1,16 @@
 from pyeda.inter import *
 
-def number_to_boolean_formula(num):
+def number_to_boolean_formula(num: int) -> str:
     """
-    Given a base ten number return the boolean formula for that number
+    Given a base ten number return the boolean formula 
+    (represented in string form) for that number
     """
-    # covert the base 10 to five digit binary
+    #NOTE: the reason that the number is in string for is it makes it easier
+    #      to add multiple boolean formulas together, which is needed later
+    
+    # covert the base 10 number to five digit binary
     binary_num = f"{num:05b}"
-    print(binary_num)
+    # print(binary_num)
 
     # loop through binary to create the boolean formula
     boolean_formula = ""
@@ -20,12 +24,14 @@ def number_to_boolean_formula(num):
             print("Something has gone wrong")
 
         position += 1
+
+    # remove the last (hanging) ampersand and return
     boolean_formula = boolean_formula[:-1]
-    print(expr(boolean_formula))
+    return boolean_formula
 
 # even and prime arrays
-even = [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30]
+even = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
 prime = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
 
 # print(number_to_boolean_formula(3));
-number_to_boolean_formula(3);
+print(number_to_boolean_formula(10))
