13c5822876efa1563619ebac62dafffeb2df3006
Cass Dahle <isaacbd.personal@gmail.com>
2023-11-04 21:47:25 -0700

    added some print statements

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ edges = [(i,j) for i in range(32) for j in range(32) if (i+3)%32 is j%32 or (i+8
 
 # get the BDD for the set of even numbers
 even_boolean_formula = list_to_boolean_formula(even, 'y')
-print(even_boolean_formula)
+# print(even_boolean_formula)
 even_BDD = expr2bdd(even_boolean_formula)
 
 # get the BDD for the set of prime numbers
 prime_boolean_formula = list_to_boolean_formula(prime, 'x')
+# print(prime_boolean_formula)
 prime_BDD = expr2bdd(prime_boolean_formula)
 
 # get the BDD for the set of edges
 edge_boolean_formula = edge_list_to_boolean_formula(edges)
+# print(edge_boolean_formula)
 edge_BDD = expr2bdd(edge_boolean_formula)
